374c62d93a127718a6102691288d69cde38279e8
Andre Smith <andre.frank.smith@gmail.com>
2026-02-12 11:08:31 +0200

    Lesson 3.1 Implement first libraries

---
+++ libs/tasks/feature/src/lib/feature/feature.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-feature',
+  imports: [],
+  templateUrl: './feature.html',
+  styleUrl: './feature.css',
+})
+export class Feature {}
